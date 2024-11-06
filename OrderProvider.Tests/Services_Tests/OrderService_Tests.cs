bf1307d7d6dfa9ba3c46038d7cd2c8ed14ab86a8
tim mårtensson <tim.martensson92@gmail.com>
2024-11-06 22:07:58 +0100

    first test done!

--- OrderProvider.Tests/Services_Tests/OrderService_Tests.cs
+++ OrderProvider.Tests/Services_Tests/OrderService_Tests.cs
@@ -1,5 +1,7 @@
 ﻿using Moq;
+using OrderProvider.Entities;
 using OrderProvider.Interfaces;
+using OrderProvider.Models;
 using OrderProvider.Services;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace OrderProvider.Tests.Services_Tests;
 
-internal class OrderService_Tests
+public class OrderService_Tests
 {
     private readonly Mock<IOrderRepo> _repo;
     private readonly IOrderService _service;
@@ -23,6 +25,50 @@ internal class OrderService_Tests
     [Fact]
     public async Task Should_Create_order_AndReturnTrue()
     {
+        // Arrange
+        var orderRequest = new OrderRequest
+        {
+            CustomerId = "1",
+            CustomerEmail = "test@example.com",
+            OrderItems = new List<OrderItemDto>
+            {
+                new OrderItemDto
+                {
+                    ProductId = "1",
+                    ProductName = "Product 1",
+                    Size = "M",
+                    Color = "Red",
+                    Quantity = 1,
+                    Price = 100
+                },
+                new OrderItemDto
+                {
+                    ProductId = "2",
+                    ProductName = "Product 2",
+                    Size = "L",
+                    Color = "Blue",
+                    Quantity = 2,
+                    Price = 200
+                }
+            }
+        };
 
+        _repo.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<OrderEntity>());
+        _repo.Setup(repo => repo.CreateOneAsync(It.IsAny<OrderEntity>())).ReturnsAsync(new OrderEntity());
+
+        OrderEntity capturedOrderEntity = null!;
+        _repo.Setup(repo => repo.CreateOneAsync(It.IsAny<OrderEntity>()))
+            .Callback<OrderEntity>(order => capturedOrderEntity = order)
+            .ReturnsAsync(new OrderEntity());
+
+        // Act
+        var result = await _service.CreateOrderAsync(orderRequest);
+
+        // Assert
+        Assert.True(result);
+        _repo.Verify(repo => repo.GetAllAsync(), Times.Once);
+        _repo.Verify(repo => repo.CreateOneAsync(It.IsAny<OrderEntity>()), Times.Once);
+        Assert.NotNull(capturedOrderEntity);
+        Assert.Equal(1, capturedOrderEntity.OrderNumber);
     }
 }
